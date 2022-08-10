f00db245db7dbbeee56d76623ed41ead3f0532d5
Ruan Carlos <ruancarlos.s@hotmail.com>
2022-08-09 23:48:24 -0300

    feat: componente de tema

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColorWhite = Color(0xFFFFFFFF);
+  static const Color primaryColor87 = Color(0xDD000000);
+  static const Color primaryColor54 = Color(0x8A000000);
+  static const Color secondaryColor = Colors.yellow;
+}
---
+++ lib/theme/app_typography.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+TextStyle titleCategoryStyle = const TextStyle(
+  fontWeight: FontWeight.w700,
+  color: AppColors.secondaryColor,
+  fontSize: 16,
+);
+
+TextStyle titleVideoStyle = const TextStyle(
+  fontWeight: FontWeight.w700,
+  color: AppColors.primaryColorWhite,
+  fontSize: 16,
+);
+
+TextStyle descriptionStyle = const TextStyle(
+  fontWeight: FontWeight.w400,
+  color: AppColors.primaryColorWhite,
+  fontSize: 14,
+);
